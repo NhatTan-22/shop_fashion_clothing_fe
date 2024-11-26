2cdc27fd1cbfe1f3e5dc50664667ec0e39a9ba83
NhatTan_Dev <tandevolopment.fe@gmail.com>
2024-11-27 04:00:27 +0700

    code route and auth layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@
 //#endregion Specific Component
 
 //#region Common Component
-// export { default as NotFoundPage } from './common/notFound/NotFound';
+export { default as NotFoundPage } from './common/notFound_404/NotFound';
 export { default as GlobalStyles } from './common/globalStyles/GlobalStyles';
 export { default as Loading } from './common/loading/Loading';
 //#endregion Common Component
