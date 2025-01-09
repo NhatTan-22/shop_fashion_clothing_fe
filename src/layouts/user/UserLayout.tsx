f24b816e52792c53358af49994c4c36bd049c836
NhatTan_Dev <tandevolopment.fe@gmail.com>
2025-01-09 23:57:29 +0700

    UI popular product

--- src/layouts/user/UserLayout.tsx
+++ src/layouts/user/UserLayout.tsx
@@ -1,7 +1,7 @@
 // Libs
 import classNames from 'classnames/bind';
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 // Components, Layouts, Pages
 import { BaseButton } from '~/components';
@@ -32,7 +32,7 @@ const UserLayout = (props: Props) => {
     //#region Declare State
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
-
+    const [isOpenSideBae, setIsOpenSidebar] = useState<boolean>(true);
     //#endregion Declare State
 
     //#region Implement Hook
@@ -53,9 +53,29 @@ const UserLayout = (props: Props) => {
             path: '/contact',
         },
     ];
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
     //#endregion Implement Hook
 
     //#region Handle Function
+    const handleResize = () => {
+        if (window.innerWidth <= 1023) {
+            setIsOpenSidebar(false);
+        } else {
+            setIsOpenSidebar(true);
+        }
+    };
+
+    const handleSideBar = () => {
+        setIsOpenSidebar(!isOpenSideBae);
+    };
+
     const handleClick = (event: any) => {
         setAnchorEl(event.currentTarget);
     };
@@ -91,83 +111,96 @@ const UserLayout = (props: Props) => {
 
                 <div className={cx('navigateHeader')}>
                     <Link to='/'>
-                        <div className='w-72'>
+                        <div className='w-[200px] mr-6'>
                             <img className='w-24 h-24 object-fill' alt='LOGO_SHOP' src={images.logoFashionStore} />
                         </div>
                     </Link>
-                    <div className='flex w-full'>
-                        <ul className='flex justify-between items-center '>
-                            {listHeader.map((header) => (
-                                <Link key={header.path} to={header.path}>
-                                    <li className='mr-8 pb-0 hover:text-red-600 '>
-                                        <BaseButton styleButton={ButtonStyleEnum.TEXT}>
-                                            <span>{t(`${header.title}`)}</span>
-                                            {header.icon}
-                                        </BaseButton>
-                                    </li>
-                                </Link>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className={cx('actionUser')}>
-                        <form action='/' className='relative'>
-                            <input
-                                // onChange=""
-                                className={cx('inputSearch')}
-                                type='text'
-                                placeholder='Search our store...'
-                            />
-                            <div className='absolute top-0 right-6'>
-                                <BaseButton
-                                    styleButton={ButtonStyleEnum.TEXT}
-                                    nextIcon={`${icons.searchIcon}`}
-                                    className='focus:text-blue-500'
-                                />
+                    {isOpenSideBae ? (
+                        <>
+                            <div className={cx('navigate')}>
+                                <ul className='flex justify-between items-center '>
+                                    {listHeader.map((header) => (
+                                        <Link key={header.path} to={header.path}>
+                                            <li className='mr-8 pb-0 hover:text-red-600 '>
+                                                <BaseButton styleButton={ButtonStyleEnum.TEXT}>
+                                                    <span>{t(`${header.title}`)}</span>
+                                                    {header.icon}
+                                                </BaseButton>
+                                            </li>
+                                        </Link>
+                                    ))}
+                                </ul>
                             </div>
-                        </form>
-                        <BaseButton styleButton={ButtonStyleEnum.TEXT}>
-                            <Link to='/wishlist'>
-                                <img src={icons.heartIcon} alt='' />
-                            </Link>
-                        </BaseButton>
-                        <BaseButton styleButton={ButtonStyleEnum.TEXT}>
-                            <div className='relative'>
-                                <Link to='order'>
-                                    <img src={icons.cartIcon} alt='' />
-                                    <div className='absolute top-2 left-3 rounded-full bg-wisteria-600'>
-                                        <span className='px-2 text-white'>0</span>
+                            <div className={cx('actionUser')}>
+                                <form action='/' className='relative'>
+                                    <input
+                                        // onChange=""
+                                        className={cx('inputSearch')}
+                                        type='text'
+                                        placeholder='Search our store...'
+                                    />
+                                    <div className='absolute top-0 right-6'>
+                                        <BaseButton
+                                            styleButton={ButtonStyleEnum.TEXT}
+                                            nextIcon={`${icons.searchIcon}`}
+                                            className='focus:text-blue-500'
+                                        />
                                     </div>
+                                </form>
+                                <BaseButton styleButton={ButtonStyleEnum.TEXT}>
+                                    <Link to='/wishlist'>
+                                        <img src={icons.heartIcon} alt='' />
+                                    </Link>
+                                </BaseButton>
+                                <BaseButton styleButton={ButtonStyleEnum.TEXT}>
+                                    <div className='relative'>
+                                        <Link to='order'>
+                                            <img src={icons.cartIcon} alt='' />
+                                            <div className='absolute top-2 left-3 rounded-full bg-wisteria-600'>
+                                                <span className='px-2 text-white'>0</span>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                </BaseButton>
+                                {/* <BaseButton styleButton={ButtonStyleEnum.TEXT}>
+                                <img
+                                    className='w-10 h-10 rounded-full'
+                                    src='https://th.bing.com/th/id/OIP.Gg0lRdcH7S-EO2NWbRzCMQAAAA?pid=ImgDet&w=167&h=183&c=7&dpr=1.3'
+                                    alt=''
+                                />
+                                    </BaseButton> */}
+                                <Link to='/auth/login'>
+                                    <BaseButton nameButton='Login' />
                                 </Link>
+                                <Link to='/auth/register'>
+                                    <BaseButton nameButton='Register' />
+                                </Link>
+                                {/* <Menu
+                                    id='basic-menu'
+                                    // anchorEl={anchorEl}
+                                    // open={open}
+                                    // onClose={handleClose}
+                                    // MenuListProps={{
+                                    //     'aria-labelledby': 'basic-button',
+                                    // }}
+                                >
+                                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                                    <Link to='/auth/login'>
+                                        <MenuItem>Logout</MenuItem>
+                                    </Link>
+                                </Menu> */}
                             </div>
-                        </BaseButton>
-                        {/* <BaseButton styleButton={ButtonStyleEnum.TEXT}>
-                            <img
-                                className='w-10 h-10 rounded-full'
-                                src='https://th.bing.com/th/id/OIP.Gg0lRdcH7S-EO2NWbRzCMQAAAA?pid=ImgDet&w=167&h=183&c=7&dpr=1.3'
-                                alt=''
-                            />
-                        </BaseButton> */}
-                        <Link to='/auth/login'>
-                            <BaseButton nameButton='Login' />
-                        </Link>
-                        <Link to='/auth/register'>
-                            <BaseButton nameButton='Register' />
-                        </Link>
-                        {/* <Menu
-                        id='basic-menu'
-                        // anchorEl={anchorEl}
-                        // open={open}
-                        // onClose={handleClose}
-                        // MenuListProps={{
-                        //     'aria-labelledby': 'basic-button',
-                        // }}
-                    >
-                        <MenuItem onClick={handleClose}>My account</MenuItem>
-                        <Link to='/auth/login'>
-                            <MenuItem>Logout</MenuItem>
-                        </Link>
-                    </Menu> */}
-                    </div>
+                        </>
+                    ) : (
+                        <img
+                            className={cx('listMenu')}
+                            width={24}
+                            height={24}
+                            src={icons.listMenuIcon}
+                            alt=''
+                            onClick={handleSideBar}
+                        />
+                    )}
                 </div>
             </div>
 
@@ -175,8 +208,8 @@ const UserLayout = (props: Props) => {
                 <Outlet />
             </div>
 
-            <div className='w-full py-16 bg-bossa-nova-900 text-white'>
-                <div className='grid grid-flow-col gap-28 max-w-screen-2xl mx-auto h-44'>
+            <div className={cx('footer')}>
+                <div className={cx('contentFooter')}>
                     <div>
                         <img className='w-32 h-32 p-6 rounded-full' alt='LOGO_SHOP' src={images.fashionStore} />
                         <p>©2024 Nhat Tan | Built with by Nhat Tan.</p>
