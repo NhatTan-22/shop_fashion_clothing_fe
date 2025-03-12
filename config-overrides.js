c9f2514d8df3556bedd6b213571d997605187b5e
NhatTan_Dev <tandevolopment.fe@gmail.com>
2025-03-12 21:25:15 +0700

    Config and build

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,25 @@
-const { override, useBabelRc } = require('customize-cra');
+const { override, addWebpackModuleRule, addWebpackAlias } = require('customize-cra');
+const path = require('path');
 
 module.exports = override(
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useBabelRc()
+  // Cấu hình alias cho Webpack để ánh xạ `~` tới thư mục `src`
+  addWebpackAlias({
+    '~': path.resolve(__dirname, 'src'),
+  }),
+
+  // Thêm cấu hình SCSS với resolve-url-loader và sass-loader
+  addWebpackModuleRule({
+    test: /\.scss$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'resolve-url-loader',  // Xử lý URL trong SCSS
+      {
+        loader: 'sass-loader',
+        options: {
+          sourceMap: true, // Đảm bảo là boolean true
+        },
+      },
+    ],
+  })
 );
