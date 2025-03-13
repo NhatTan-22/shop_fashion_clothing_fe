f869ce75b70939e3936ae4a62aa4454570978915
NhatTan_Dev <tandevolopment.fe@gmail.com>
2025-03-13 23:14:17 +0700

    Build

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,6 @@
-const { override, useBabelRc, addWebpackModuleRule } = require('customize-cra');
+const { override, useBabelRc } = require("customize-cra");
 
 module.exports = override(
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useBabelRc(),
-  addWebpackModuleRule({
-    test: /\.scss$/i,
-    use: [
-      'style-loader',
-      'css-loader',
-      'sass-loader',
-    ],
-  })
 );
